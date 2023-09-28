0974ee4895baf144bec63ff9685ffd7f5a58d311
Matthias Kretz <m.kretz@gsi.de>
2023-09-28 20:48:27 +0200

    Add std::simd based peak FLOP benchmark
    ChangeLog:
    
    	* CMakeLists.txt: Add peakflop-stdsimd benchmark.
    	* peakflop-stdsimd.cpp: New file.
    	* peakflop.cpp: Use range-based for. Move fake_read after the
    	benchmark loop.

--- peakflop-stdsimd.cpp
+++ peakflop-stdsimd.cpp
@@ -1,25 +1,26 @@
 #include "benchmark.h"
-#include <vir/simd.h>
+#include <../simd-prototyping/simd.h>
 #include <vir/simd_benchmarking.h>
 
 namespace stdx = vir::stdx;
 
+using V = std::simd<float, std::simd<float>::size() * 8>;
+
+void do_one(V& v)
+{
+  v = v * 3.f + 1.f;
+}
+
+
 void peak(benchmark::State &state)
 {
-  constexpr int N = 8;
-  using V = stdx::native_simd<float>;
-  V x[N] = {};
-  for (auto& v : x) {
-    vir::fake_modify(v);
-  }
+  constexpr int N = 1;
+  V x = {};
+  asm("" : "+m"(x));
   for (auto _ : state) {
-    for (auto& v : x) {
-      v = v * 3 + 1;
-    }
+    x = x * 3.f + 1.f;
   }
-  for (auto& v : x) {
-    vir::fake_read(v);
-  }
+  asm("" :: "m"(x));
   // compute FLOP/s and FLOP/cycle
   add_flop_counters(state, 2 * N * element_count<V>::value);
 }
